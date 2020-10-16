5ca518cd9bd13a4dd667b5ffba5e85cbf3947d9f
felix1234567890 <blueskyandsea01@gmail.com>
2020-10-16 07:42:57 +0200

    Rendering cards in layout

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Form\MovieType;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +12,13 @@ class MovieController extends AbstractController
 {
     /**
      * @Route("/", name="movies")
+     * @param MovieRepository $movieRepository
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(MovieRepository $movieRepository)
     {
-        return $this->render('movie/index.html.twig');
+        $movies = $movieRepository->findAll();
+        return $this->render('movie/index.html.twig', ['movies' => $movies]);
     }
 
     /**
